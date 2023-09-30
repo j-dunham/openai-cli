8ccb7f8d98c21e15e2bc859cae37663c70aee4a3
Simon Sanchez <simonsanchez@github.com>
2023-09-29 17:06:46 -0700

    Add `Config` struct

---
+++ config/config.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	OpenAiToken       string
+	OpenAiModel       string
+	OpenAiMaxTokens   int
+	OpenAiTemperature float64
+}
+
+func NewConfig() (*Config, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, err
+	}
+
+	if os.Getenv("OPEN_AI_TOKEN") == "" {
+		return nil, fmt.Errorf("OPEN_AI_TOKEN is not set")
+	}
+
+	if os.Getenv("OPEN_AI_MODEL") == "" {
+		return nil, fmt.Errorf("OPEN_AI_MODEL is not set")
+	}
+
+	if os.Getenv("OPEN_AI_MAX_TOKENS") == "" {
+		return nil, fmt.Errorf("OPEN_AI_MAX_TOKENS is not set")
+	}
+
+	if os.Getenv("OPEN_AI_TEMPERATURE") == "" {
+		return nil, fmt.Errorf("OPEN_AI_TEMPERATURE is not set")
+	}
+
+	maxTokens, err := strconv.Atoi(os.Getenv("OPEN_AI_MAX_TOKENS"))
+	if err != nil {
+		return nil, err
+	}
+
+	temperature, err := strconv.ParseFloat(os.Getenv("OPEN_AI_TEMPERATURE"), 64)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		OpenAiToken:       os.Getenv("OPEN_AI_TOKEN"),
+		OpenAiModel:       os.Getenv("OPEN_AI_MODEL"),
+		OpenAiMaxTokens:   maxTokens,
+		OpenAiTemperature: temperature,
+	}, nil
+}
